11c00b058e59301ff336c8bd3682a4a94f5dc873
TothKristof <56380808+TothKristof@users.noreply.github.com>
2026-01-23 10:37:56 +0100

    List recently added items on main page

---
+++ app/main/main.styles.ts
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled";
+
+export const PageWrapper = styled.div({
+  display: "flex",
+  flexDirection: 'column',
+  width: "100%",
+  backgroundColor: 'rgb(230, 209, 199)',
+  gap: 24,
+});
+
+export const Sidebar = styled.div({
+  width: 220,
+  backgroundColor: "#ffffff",
+  borderRadius: 16,
+});
+
+export const ContentWrapper = styled.div({
+  flex: 1,
+  display: "flex",
+  gap: 24,
+});
+
+export const MainColumn = styled.div({
+  flex: 3,
+  display: "flex",
+  flexDirection: "column",
+  gap: 24,
+});
+
+export const RightColumn = styled.div({
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  gap: 24,
+});
+
+export const CardRow = styled.div({
+  display: "flex",
+  gap: 24,
+});
+
+export const Card = styled.div({
+  flex: 1,
+  height: 200,
+  backgroundColor: "#ffffff",
+  borderRadius: 16,
+});
